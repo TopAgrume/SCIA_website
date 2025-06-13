03e66efdc94bd2ffa9db165369e522ff1734818b
Rick GAO <rick.gao@epita.fr>
2025-06-13 14:53:01 +0200

    fix(actions.ts): return errorMessage object in authenticate function

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -16,9 +16,9 @@ export async function authenticate(
     if (error instanceof AuthError) {
       switch (error.type) {
         case 'CredentialsSignin':
-          return 'Invalid credentials.';
+          return { errorMessage: 'Invalid credentials.' };
         default:
-          return 'Something went wrong.';
+          return { errorMessage: 'Something went wrong.' };
       }
     }
     throw error;
